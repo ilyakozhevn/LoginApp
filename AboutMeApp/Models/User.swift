f41e6ab317550d0f6be0acd3e89589bec2f96075
ilyakozhevn <kozhevilya@gmail.com>
2022-07-07 12:27:08 +0300

    getMethods for all structs added

--- AboutMeApp/Models/User.swift
+++ AboutMeApp/Models/User.swift
@@ -26,58 +26,8 @@ struct User {
     static func createUser(login: String) -> User {
         User(
             login: login,
-            info:
-                Info(
-                    name: "Ilya Kozhevnikov",
-                    about: "electronic artist from Russia who makes melodic deep techno. His music is for the chill out moments when you’re looking to kick back at the end of party.",
-                    references: ["Yagya", "Stephan Bodzin"],
-                    avatar: "Ilya Kozhevnikov_main3000"
-                ),
-            
-            releases: [
-                Release(
-                    albumName: "Hello World",
-                    link: "https://soundcloud.com/elliahh/hello-world",
-                    labelName: .Elliahh,
-                    tracks: ["Hello World"],
-                    image: "helloWorld"
-                ),
-                Release(
-                    albumName: "Memories",
-                    link: "https://ilya.bandcamp.com/track/memories-extended-mix",
-                    labelName: .Elliahh,
-                    tracks: ["Memories"],
-                    image: "memories"
-                ),
-                Release(
-                    albumName: "Primitive",
-                    link: "https://www.beatport.com/release/primitive/3626152",
-                    labelName: .Suprematic,
-                    tracks: ["Primitive"],
-                    image: "primitive"
-                ),
-                Release(
-                    albumName: "The End Of Time",
-                    link: "https://insectorama.bandcamp.com/album/the-end-of-time",
-                    labelName: .Insectorama,
-                    tracks: ["Hello World", "Clouds", "The End Of Time", "Miracle"],
-                    image: "theEndOfTime"
-                ),
-                Release(
-                    albumName: "Rest State",
-                    link: "https://www.beatport.com/release/rest-state/3659521",
-                    labelName: .Suprematic,
-                    tracks: ["Rest State (Extended Mix)"],
-                    image: "restState"
-                ),
-                Release(
-                    albumName: "Try",
-                    link: "https://www.beatport.com/release/try/3722356",
-                    labelName: .Suprematic,
-                    tracks: ["Try (Extended Mix)"],
-                    image: "try"
-                )
-            ]
+            info: Info.getInfo(),
+            releases: Release.getReleases()
         )
     }
 }
@@ -87,6 +37,15 @@ struct Info {
     var about: String
     var references: [String]
     var avatar: String
+    
+    static func getInfo() -> Info {
+        Info(
+            name: "Ilya Kozhevnikov",
+            about: "electronic artist from Russia who makes melodic deep techno. His music is for the chill out moments when you’re looking to kick back at the end of party.",
+            references: ["Yagya", "Stephan Bodzin"],
+            avatar: "Ilya Kozhevnikov_main3000"
+        )
+    }
 }
 
 struct Release {
@@ -95,6 +54,53 @@ struct Release {
     var labelName: Labels
     var tracks: [String]
     var image: String
+    
+    static func getReleases() -> [Release] {
+        [
+            Release(
+                albumName: "Hello World",
+                link: "https://soundcloud.com/elliahh/hello-world",
+                labelName: .Elliahh,
+                tracks: ["Hello World"],
+                image: "helloWorld"
+            ),
+            Release(
+                albumName: "Memories",
+                link: "https://ilya.bandcamp.com/track/memories-extended-mix",
+                labelName: .Elliahh,
+                tracks: ["Memories"],
+                image: "memories"
+            ),
+            Release(
+                albumName: "Primitive",
+                link: "https://www.beatport.com/release/primitive/3626152",
+                labelName: .Suprematic,
+                tracks: ["Primitive"],
+                image: "primitive"
+            ),
+            Release(
+                albumName: "The End Of Time",
+                link: "https://insectorama.bandcamp.com/album/the-end-of-time",
+                labelName: .Insectorama,
+                tracks: ["Hello World", "Clouds", "The End Of Time", "Miracle"],
+                image: "theEndOfTime"
+            ),
+            Release(
+                albumName: "Rest State",
+                link: "https://www.beatport.com/release/rest-state/3659521",
+                labelName: .Suprematic,
+                tracks: ["Rest State (Extended Mix)"],
+                image: "restState"
+            ),
+            Release(
+                albumName: "Try",
+                link: "https://www.beatport.com/release/try/3722356",
+                labelName: .Suprematic,
+                tracks: ["Try (Extended Mix)"],
+                image: "try"
+            )
+        ]
+    }
 }
 
 enum Labels: String {
